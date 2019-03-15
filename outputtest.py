b45b8d54b58e979516114193d8e4c518a1ea7795
Ahmed <ahsherif@microsoft.com>
2019-03-15 13:00:14 -0400

    update for dill input output serialization

---
+++ outputtest.py
@@ -0,0 +1,42 @@
+# run.log('regularization rate', np.float(args.reg))
+# run.log('accuracy', np.float(mean((scores))))
+import os
+
+import dill
+import pickle
+import pandas as pd
+from azureml.core import Run
+# from textblob import TextBlob
+from statistics import mean
+
+# let user feed in 2 parameters, the location of the data files (from datastore), and the regularization rate of the logistic regression model
+
+# load train and test set into numpy arrays
+sample_data = [(1,'This is awesome'), (2, 'This is average'), (3, 'This is awful')]
+
+df = pd.DataFrame(sample_data, columns = ['no', 'text'])
+
+print('Test a textblob model')
+
+############
+
+def textblob_model(text):
+    from textblob import TextBlob
+    a = TextBlob(text).sentiment.polarity
+    return a
+############
+
+
+# calculate accuracy on the prediction
+scores = []
+for i in df['text']:
+    print(textblob_model(i))
+    scores.append(textblob_model(i))
+
+print(f'The average accuracy: {mean((scores))}')
+
+os.makedirs('outputs', exist_ok=True)
+# note file saved in the outputs folder is automatically uploaded into experiment record
+filename = 'outputs/ta_model.pkl'
+dill.dump(textblob_model, open(filename, 'wb'))
+# pickle.dumps(textblob_model, open(filename, 'wb'))
--- sentimentProgram.py
+++ sentimentProgram.py
@@ -1,4 +1,7 @@
-import pandas as pd 
+from statistics import mean
+
+import dill
+import pandas as pd
 from textblob import TextBlob
 
 sample_data = [(1,'This is awesome'), (2, 'This is average'), (3, 'This is awful')]
@@ -7,17 +10,22 @@ df = pd.DataFrame(sample_data, columns = ['no', 'text'])
 
 df['sentiment'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
 
-import dill
 
 def textblob_model(text):
     a = TextBlob(text).sentiment.polarity
     return a
 
 
-tbmodel = dill.dumps(textblob_model)
+# tbmodel = dill.dumps(textblob_model)
 
-serialized_dill_model = dill.loads(tbmodel)
+# serialized_dill_model = dill.loads(tbmodel)
 
-serialized_dill_model('This is ok')
+# serialized_dill_model('This is ok')
 
+scores = []
+for i in df['text']:
+    print(textblob_model(i))
+    scores.append(textblob_model(i))
 
+print(f'accuracy: {mean((scores))}')
+print(scores)
